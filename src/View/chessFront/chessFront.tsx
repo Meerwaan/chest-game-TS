74d94d759187f952d000c99b25a863c85fe6a0d5
fayssal <fayssalmechmeche.pro@gmail.com>
2023-04-19 13:37:55 +0200

    remove code

--- src/View/chessFront/chessFront.tsx
+++ src/View/chessFront/chessFront.tsx
@@ -1,41 +1,20 @@
-import React, { useState } from "react";
-import {Chess} from "chess.js";
 import Chessground from "@react-chess/chessground";
+
 import "chessground/assets/chessground.base.css";
 import "chessground/assets/chessground.brown.css";
 import "chessground/assets/chessground.cburnett.css";
 
-interface ChessBoardProps {
-  fen: string;
-  onMove: ({ from, to }: { from: string; to: string }) => void;
-  onDrop: ({ sourceSquare, targetSquare }: any) => "snapback" | undefined;
+import {Chess} from "chess.js";
+import React from "react";
+
+
+function ChessFront() {
+  const options = {
+    // options de configuration
+  };
+  const game = new Chess(); 
+
+  return <Chessground {...options} />;
 }
 
-function Chessboard() {
-  const [fen, setFen] = useState("start");
-  const [game, setGame] = useState(new Chess());
-
-  const onMove = ({ from, to }: { from: string; to: string }) => {
-    const move = game.move({ from, to, promotion: "q" });
-    if (move === null) return;
-    setFen(game.fen());
-  };
-
-  const onDrop = ({ sourceSquare, targetSquare }: any) => {
-    const move = game.move({
-      from: sourceSquare,
-      to: targetSquare,
-      promotion: "q",
-    });
-    if (move === null) return "snapback";
-    setFen(game.fen());
-  };
-
-  return (
-    <div>
-      <Chessground  onMove={onMove} onDrop={onDrop} />
-    </div>
-  );
-}
-
-export default Chessboard;
+export default ChessFront;
